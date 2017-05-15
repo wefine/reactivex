8f6bb02c2b2893baac6acd99a406b2ae10f54d00
wefine <wefine@aliyun.com>
2017-05-15 21:32:55 +0800

    add aeron official example.
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

---
+++ aeron-examples/src/main/java/io/aeron/samples/LowLatencyMediaDriver.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2014-2017 Real Logic Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.aeron.samples;
+
+import io.aeron.driver.MediaDriver;
+import io.aeron.driver.ThreadingMode;
+import org.agrona.concurrent.BusySpinIdleStrategy;
+import org.agrona.concurrent.ShutdownSignalBarrier;
+
+/**
+ * Sample setup for a {@link MediaDriver} that is configured for low latency communications.
+ */
+public class LowLatencyMediaDriver {
+    public static void main(final String[] args) throws Exception {
+        MediaDriver.loadPropertiesFiles(args);
+
+        final MediaDriver.Context ctx = new MediaDriver.Context()
+                .termBufferSparseFile(false)
+                .threadingMode(ThreadingMode.DEDICATED)
+                .conductorIdleStrategy(new BusySpinIdleStrategy())
+                .receiverIdleStrategy(new BusySpinIdleStrategy())
+                .senderIdleStrategy(new BusySpinIdleStrategy());
+
+        try (MediaDriver ignored = MediaDriver.launch(ctx)) {
+            new ShutdownSignalBarrier().await();
+
+            System.out.println("Shutdown Driver...");
+        }
+    }
+}
